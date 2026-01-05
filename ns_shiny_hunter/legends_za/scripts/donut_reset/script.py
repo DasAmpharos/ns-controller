15ddb3db8cb4e64a0c29b1ec933422b5a97f3864
Dylan Meadows <dylan_r_meadows@homedepot.com>
2026-01-05 12:52:40 -0500

    Fixing issue with imports in donut reset script

--- ns_shiny_hunter/legends_za/scripts/donut_reset/script.py
+++ ns_shiny_hunter/legends_za/scripts/donut_reset/script.py
@@ -4,9 +4,9 @@ from typing import Final
 
 import pytesseract
 
-from ns_controller.client import NsControllerGrpcClient
+from ns_controller.client.client import NsControllerClient
 from ns_controller.pb.ns_controller_pb2 import Button
-from ns_shiny_hunter.frame import FrameProcessors, Frame
+from ns_shiny_hunter.frame import Frame, FrameProcessors
 from ns_shiny_hunter.frame_grabber import FrameGrabber
 from ns_shiny_hunter.legends_za.frames import LegendsZAReferenceFrames
 from ns_shiny_hunter.legends_za.scripts.donut_reset.frames import DonutResetReferenceFrames
@@ -26,7 +26,8 @@ class DonutResetScript:
     )
     PYTESSERACT_CONFIG: Final = "--oem 3 --psm 7 -c tessedit_char_whitelist=\" .:()123ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\""
 
-    def __init__(self, frame_grabber: FrameGrabber, controller: NsControllerGrpcClient, targets: list[str], resets: int = 0):
+    def __init__(self, frame_grabber: FrameGrabber, controller: NsControllerClient, targets: list[str],
+                 resets: int = 0):
         self.frame_grabber = frame_grabber
         self.controller = controller
         self.targets = targets
@@ -145,4 +146,3 @@ class DonutResetScript:
                  and sparkling_power_effect[1] == 3)
                 and (alpha_power_effect is not None
                      and alpha_power_effect >= 2))
-
